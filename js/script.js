3aef3c27130b1d9bab0e2205c7d39228cf4fc913
Chris Holston <chrisholston@me.com>
2020-10-27 13:10:54 -0500

    updated getData function

--- js/script.js
+++ js/script.js
@@ -6,25 +6,25 @@ function handleGetData(event){
 
   $('form').on("submit", handleGetData)
 
-  //       .then(
-//         (data) => {
-//           console.log(data);
+        .then(
+        (data) => {
+          console.log(data);
           
-//           let $blank = $("#song");
-//           $blank.text("Song is " + data.name);
+          let $blank = $("#song");
+          $blank.text("Song is " + data.name);
           
-//           let $artist = $("#artist");
-//           $artist.text("Artist Name is" + data.main.artist);
+          let $artist = $("#artist");
+          $artist.text("Artist Name is" + data.main.artist);
          
-//           let $title =$("#title");
-//           $title.text("The title of the song is "  + data.main.title);
+          let $title =$("#title");
+          $title.text("The title of the song is "  + data.main.title);
   
-//           let $album =$("#album");
-//           $album.text("The album title is "  + data.main.album);
-//         },
-//         (error) => {
-//           console.log("bad request: ", error)
-//         }
-//       )
-//   }
+          let $album =$("#album");
+          $album.text("The album title is "  + data.main.album);
+        },
+        (error) => {
+          console.log("bad request: ", error)
+        }
+      )
+  }
   
