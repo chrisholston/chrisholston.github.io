681660d4cc13853cfa056826bb2b0201527c237d
Chris Holston <chrisholston@me.com>
2020-10-27 21:12:14 -0500

    set up api

--- js/script.js
+++ js/script.js
@@ -1,33 +1,49 @@
-
+$('p').addClass('text-center');
 $('h1').addClass('text-center');
-
+$('search').addClass('btn-success');
+$('form').addClass('text-center');
 function handleGetData(event){
     event.preventDefault();
     // Load in the value of the search textbox:
     const searchText = $("#search").val()
-    $.ajax({url:`http://api.openweathermap.org/data/2.5/weather?q=${searchText}&units=imperial&APPID=beb365ff76ed8b50dd570a7cef7bb822` })
-
+    console.log(searchText);
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://shazam.p.rapidapi.com/search?locale=en-US&offset=0&limit=5&term=" + searchText,
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "shazam.p.rapidapi.com",
+        "x-rapidapi-key": "f4d2747f12msh60e62a78f607b93p1cd5cajsnd8d8505fa600"
+      }
+    }
+    $.ajax(settings).done(function (response) {
+      console.log(response.tracks.hits[0]);
+      let $blank = $("#song");
+      $blank.text("Song is " + response.tracks.hits[0]);
+    });
+  }
   $('form').on("submit", handleGetData)
 
-        .then(
-        (data) => {
-          console.log(data);
+
+  //       .then(
+  //       (data) => {
+  //         console.log(data);
           
-          let $blank = $("#song");
-          $blank.text("Song is " + data.name);
+        //  let $blank = $("#song");
+        // $blank.text("Song is " + response.tracks.hits[0]);
           
-          let $artist = $("#artist");
-          $artist.text("Artist Name is" + data.main.artist);
+  //         let $artist = $("#artist");
+  //         $artist.text("Artist Name is" + data.main.artist);
          
-          let $title =$("#title");
-          $title.text("The title of the song is "  + data.main.title);
+  //         let $title =$("#title");
+  //         $title.text("The title of the song is "  + data.main.title);
   
-          let $album =$("#album");
-          $album.text("The album title is "  + data.main.album);
-        },
-        (error) => {
-          console.log("bad request: ", error)
-        }
-      )
-  }
-  
+  //         let $album =$("#album");
+  //         $album.text("The album title is "  + data.main.album);
+  //       },
+  //       (error) => {
+  //         console.log("bad request: ", error)
+  //       }
+  //     )
+  // }
